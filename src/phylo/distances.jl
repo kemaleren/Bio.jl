b79784cb97758a3178e87b823f28047b5b0bc4bc
Ben J. Ward <axolotlfan9250@gmail.com>
2016-07-26 10:39:35 +0100

    Initial commit evo_dist

---
+++ src/phylo/distances.jl
@@ -0,0 +1,8 @@
+# phylo/distances.jl
+# ==================
+#
+# Types and methods for computing evolutionary distances.
+#
+# Part of the Bio.Phylo module.
+#
+# This file is a part of BioJulia. License is MIT: https://github.com/BioJulia/Bio.jl/blob/master/LICENSE.md
