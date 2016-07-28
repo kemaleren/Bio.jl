554b61657fa812699ef3e793dc5b1ae2d81c043b
Ben J. Ward <axolotlfan9250@gmail.com>
2016-07-28 19:02:35 +0100

    count_differences

--- src/phylo/distances.jl
+++ src/phylo/distances.jl
@@ -8,16 +8,36 @@
 # This file is a part of BioJulia. License is MIT: https://github.com/BioJulia/Bio.jl/blob/master/LICENSE.md
 
 
-# Distance calculation infrastructure/api
-# ---------------------------------------
-
-# A type inheriting from EvoDist must implement the following methods.
-
-# computes_variance(::Type{})
-# return_type(::Type{})
+# Types
+# -----
 
 abstract EvoDist
+abstract UncorrectedDist <: EvoDist
+abstract CorrectedDist <: EvoDist
+abstract TsTv <: CorrectedDist
 
+immutable Raw <: UncorrectedDist end
+immutable JC69 <: CorrectedDist end
+immutable K80 <: TsTv end
+immutable F81 end
+immutable K81 end
+immutable F84 end
+immutable T92 end
+immutable TN93 end
+
+# Macros
+# ------
+macro pairwiseDel(expr)
+    :(if !isambiguous(a[i]) && !isambiguous(b[i])
+        nSites += 1
+        $expr
+    end)
+end
+
+# Methods
+# -------
+
+# Count the different types of difference.
 
 function count_differences(a::BioSequence, b::BioSequence, ::Type{EvoDist})
     @assert length(a) == length(b)
@@ -26,38 +46,48 @@ end
 
 function count_differences_pairdel(a::BioSequence, b::BioSequence, ::Type{EvoDist})
     @assert length(a) == length(b)
-    d = 0
-    l = 0
+    nDifferences = 0
+    nSites = 0
     for i in eachindex(a)
-        if isambiguous(a[i]) || isambiguous(b[i])
-            continue
-        else
-            l += 1
-            if a[i] != b[i]
-                d += 1
+        @pairwiseDel if a[i] != b[i]
+            nDifferences += 1
+        end
+    end
+    return nDifferences, nSites
+end
+
+function countTsTv(a::BioSequence, b::BioSequence, model::TsTv)
+    @assert length(a) == length(b)
+    nDifferences = 0
+    nTransitions = 0
+    nSites = length(a)
+    for i in eachindex(a)
+        if a[i] != b[i]
+            nd += 1
+            if (isPurine(a[i]) && isPurine(b[i])) || isPyrimidine(a[i]) && isPyrimidine(a[i])
+                nTransitions += 1
             end
         end
     end
-    return d, l
+    nTransversions = nDifferences - nTransitions
+    return nDifferences, nTransitions, nTransversions, nSites
 end
 
-
-macro pairwise(expr)
-    quote
-        r = 1
-        for i = 1:endof(seqs)
-            for j = i+1:endof(seqs)
-                res[r] = $(expr)(seqs[i], seqs[j], model)
-                r += 1
+function countTsTv_pairdel(a::BioSequence, b::BioSequence, model::TsTv)
+    @assert length(a) == length(b)
+    nDifferences = 0
+    nTransitions = 0
+    nSites = 0
+    for i in eachindex(a)
+        @pairwiseDel if a[i] != b[i]
+            nDifferences += 1
+            if (isPurine(a[i]) && isPurine(b[i])) || isPyrimidine(a[i]) && isPyrimidine(a[i])
+                nTransitions += 1
             end
         end
     end
-end
-
-macro pairdel(expr)
-    quote
-
-    end
+    nTransversions = nDifferences - nTransitions
+    return nDifferences, nTransitions, nTransversions, nSites
 end
 
 
@@ -73,56 +103,74 @@ end
 
 ## Raw distances.
 
-immutable Raw <: EvoDist end
 
-computes_variance(::Type{Raw}) = false
-gamma_correction(::Type{Raw}) = false
 
 @inline function distance(a::BioSequence, b::BioSequence, model::Raw)
-    @assert length(a) == length(b)
-    return mismatches(a, b) / length(a)
-end
-
-@inline function distance_pairdel(a::BioSequence, b::BioSequence, model::Raw)
-    @assert length(a) == length(b)
-    d = 0
-    l = 0
-    for i in eachindex(a)
-        @checkambig begin
-            if a[i] != b[i]
-                d += 1
-            end
-        end
-    end
+    d, l = count_differences(a, b, model)
     return d / l
 end
 
-#function distance_pairdel(seqs::Vector{BioSequence}, res::Vector{Float64}, model::Raw)
-#    @pairwise distance_pairdel
-#end
-#
-#function distance(seqs::Vector{BioSequence}, res::Vector{Float64}, model::Raw)
-#    @pairwise distance
-#end
-
-immutable JC69 <: EvoDist end
-
-computes_variance(::Type{JC69}) = true
-gamma_correction(::Type{JC69}) = true
-
-@inline function distance(a::BioSequence, b::BioSequence, model::JC69)
-    p = distance(a, b, Raw())
-    return -0.75 * log(1 - 4 * p / 3)
+@inline function distance_pairdel(a::BioSequence, b::BioSequence, model::Raw)
+    d, l = count_differences_pairdel(a, b, model)
+    return d / l
 end
 
-@inline function distance(a::BioSequence, b::BioSequence, model::JC69, alpha::Float64)
-    p = distance(a, b, Raw())
+
+# JC69 Distance computation.
+
+function model_correction(x::Float64, model::JC69)
+    return -0.75 * log(1 - 4 * x / 3)
 end
 
-if (*gamma)\
-  d[target] = 0.75 * *alpha*(pow(1 - 4*p/3, -1/ *alpha) - 1);\
-else d[target] = ;\
-if (*variance) {\
-    if (*gamma) var[target] = p*(1 - p)/(pow(1 - 4*p/3, -2/(*alpha + 1)) * L);\
-else var[target] = p*(1 - p)/(pow(1 - 4*p/3, 2)*L);\
-}
+function model_correction(x::Float64, gamma::Float64, model::JC69)
+    return 0.75 * alpha * ( (1 - 4 * p / 3) ^ (-1 / alpha) - 1)
+end
+
+function variance(x::Float64, L::Int, model::JC69)
+    return x * (1 - x) / (((1 - 4 * p / 3) ^ 2) * L)
+end
+
+function variance(x::Float64, L::Int, gamma::Float64, model::JC69)
+    return x * (1 - x)/(((1 - 4 * x / 3) ^ (-2 / (alpha + 1))) * L)
+end
+
+function distance(a::BioSequence, b::BioSequence, model::JC69)
+    d, l = count_differences(a, b, model)
+    p = d / l
+    D = model_correction(p, model)
+    V = variance(p, l, model)
+    return D, V
+end
+
+function distance(a::BioSequence, b::BioSequence, model::JC69, gamma::Float64)
+    d, l = count_differences(a, b, model)
+    p = d / l
+    D = model_correction(p, gamma, model)
+    V = variance(p, l, gamma, model)
+    return D, V
+end
+
+function distance_pairdel(a::BioSequence, b::BioSequence, model::JC69)
+    d, l = count_differences_pairdel(a, b, model)
+    p = d / l
+    D = model_correction(p, model)
+    V = variance(p, l, model)
+    return D, V
+end
+
+function distance_pairdel(a::BioSequence, b::BioSequence, model::JC69, gamma::Float64)
+    d, l = count_differences_pairdel(a, b, model)
+    p = d / l
+    D = model_correction(p, gamma, model)
+    V = variance(p, l, gamma, model)
+    return D, V
+end
+
+
+
+# K80 Distance computation.
+
+function distance(a::BioSequence, b::BioSequence, model::K80)
+    nd, ns, nv, l = countTsTv(a::BioSequence, b::BioSequence, model)
+
+end
